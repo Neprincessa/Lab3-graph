06dc785fe9117cd5d2f0fe73511fa79107198098
NastyaOmen <asya-iriska@yandex.ru>
2018-12-25 14:01:41 +0300

    changed

--- LAB3_Graph/graph.cpp
+++ LAB3_Graph/graph.cpp
@@ -2,31 +2,186 @@
 #include <iostream>
 #include "graph.h"
 #include "Header.h"
-using namespace std;
 
-void Graph::Build_Matrix_Adj() {
-	for (int i = 0; i <this-> amount; i++) {
-		cout << "Fill in the vertixe that is connected with " << i + 1 << " vertixe" << endl;
-		cout << "Enter numbers from 1 to " << this->amount << " If you want a vertixe doesn't connect press 0, else press 1" << endl;
-		for (int k = 0; k < this->amount; k++)
-			cout << k + 1 << " ";
-		cout << "\n";
-		int second;
-		for (int j = 0; j < this->amount; j++) {
-			cout << j + 1 << ") ";
-			second = get_flow(second, cin);
-			//cin >> second; //check
-			if (second != 0 && second != 1)
-				second = 1;
-			matrix[i][j] = second;
+const int INF = 1000000;
+
+Graph::Graph() {
+	string str;
+	cout << "Enter file name: ";
+	cin >> str;
+	loadFromFile(str);
+}//empty constructor
+
+Graph::Graph(int amountNodes) {
+	setAmount(amountNodes);
+	vector<int> buf(amount);
+	for (int i = 0; i < amount; i++) {
+		matrix.push_back(buf);
+	}
+}
+
+istream & operator >> (istream &s, Graph &it) {
+	int start, end, weigth;
+	int c;
+	do {
+		printf("Enter start apex: ");
+		get_flow_edge(start, s, it.getAmount() - 1);
+		printf("Enter end apex: ");
+		get_flow_edge(end, s, it.getAmount() - 1);
+		printf("Enter weight of rib: ");
+		get_flow(weigth, s);
+		it.addRib(start, end, weigth);
+		printf("Do you want to add more rib? (yes - 1 / no - 0)  ");
+		s >> c;
+	} while (c);
+	return s;
+}//enter ribs of graph
+
+void Graph::loadFromFile(string & name) {
+	file.open("F:/Programmes/LAB3_Graph/" + name, fstream::in | fstream::out);
+	if (!file.is_open()) {
+		throw invalid_argument("File did not find");
+	}
+	else {
+		int buf;
+		string s;
+		file >> buf;
+		setAmount(buf);
+		vector<int> b(amount);
+		for (int i = 0; i < amount; i++) {
+			matrix.push_back(b);
+		}
+		getline(file, s);
+		for (int i = 0; i < amount; i++) {
+			for (int j = 0; j < amount; j++) {
+				file >> buf;
+				file.ignore();
+				matrix[i][j] = buf;
+			}
+		}
+		file.close();
+	}
+}
+
+ostream & operator << (ostream &s, Graph &it) {
+	for (vector<int> c : it.matrix) {
+		for (int i : c)
+			s << i << ' ';
+		s << endl;
+	}
+	return s;
+}//output graph
+
+void Graph::deleteApex(int n) {
+	matrix.erase(matrix.begin() + n);
+	amount--;
+	for (int i = 0; i < amount; i++)
+		matrix[i].erase(matrix[i].begin() + n);
+}//delete apex with its ribs and all its appearance
+
+void Graph::addApex() {
+	amount++;
+	vector<int> buf(amount);
+	for (int i = 0; i < matrix.size(); i++)
+		matrix[i].push_back(0);
+	matrix.push_back(buf);
+}//add apex
+
+void Graph::deleteRib(int i, int j) {
+	
+}//delete rib between i and j apexes
+
+void Graph::addRib(int i, int j, int weight) {
+	matrix[i][j] = weight;
+}
+
+
+void Graph::floyd() {
+	int i, j, u, v;
+	d = new int*[amount];
+	next = new int*[amount];
+	for (i = 0; i < amount; i++) {
+		d[i] = new int[amount];
+		next[i] = new int[amount];
+		for (j = 0; j < amount; j++) {
+			//			if(matrix[i][j])
+			//				d[i][j] = matrix[i][j];
+			//			else
+			//				d[i][j] = INT_MAX;
+			if (matrix[i][j] || i == j)
+			{
+				next[i][j] = j;
+				d[i][j] = matrix[i][j];
+			}
+			else
+			{
+				next[i][j] = amount;
+				d[i][j] = INF;
+			}
+
 		}
 	}
 
-	cout << "Adjacency  matrix:" << endl;
-	for (int i = 0; i < amount; i++) {
-		for (int j = 0; j < amount; j++)
-			cout << matrix[i][j] << " ";
-		cout << "\n";
+	for (i = 0; i < amount; i++)
+		for (u = 0; u < amount; u++)
+			for (v = 0; v < amount; v++) {
+				if (d[u][i] + d[i][v] < d[u][v]) {
+					d[u][v] = d[u][i] + d[i][v];
+					next[u][v] = next[u][i];
+				}
+			}
+	cout << "Matrix of distances:\n";
+	for (i = 0; i < amount; i++) {
+		for (j = 0; j < amount; j++)
+			cout << d[i][j] << ' ';
+		cout << endl;
 	}
-
+	cout << "Matrix of ways:\n";
+	for (i = 0; i < amount; i++) {
+		for (j = 0; j < amount; j++)
+			cout << next[i][j] << ' ';
+		cout << endl;
+	}
 }
+
+//void floid()
+//{
+//	// инициализация матриц путей и расстояний
+//	for ( int u = 0; u < n; ++u )
+//		for ( int v = 0; v < n; ++v )
+//			if ( adj[ u ][ v ] || u == v )
+//			{
+//				path[ u ][ v ] = v;
+//				dist[ u ][ v ] = adj[ u ][ v ];
+//			}
+//			else
+//			{
+//				path[ u ][ v ] = N;
+//				dist[ u ][ v ] = INF;
+//			}
+//
+//	// сам алгоритм флойда
+//	for ( int k = 0; k < n; ++k )
+//		for ( int u = 0; u < n; ++u )
+//			if ( dist[ u ][ k ] != INF )
+//				for ( int v = 0; v < n; ++v )
+//					if ( dist[ u ][ v ] > dist[ u ][ k ] + dist[ k ][ v ] )
+//					{
+//						dist[ u ][ v ] = dist[ u ][ k ] + dist[ k ][ v ];
+//						path[ u ][ v ] = path[ u ][ k ];
+//					}
+//}
+
+
+void Graph::getShortestPath(int u, int v) {
+	if (d[u][v] == INF)
+		printf("No path found");
+	int c = u;
+	while (c != v) {
+		printf("%d -> ", c);
+		c = next[c][v];
+	}
+	printf("%d", v);
+}
+
+
